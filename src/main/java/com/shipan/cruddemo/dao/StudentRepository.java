118b1bcb11790b8128cc9f578db93e4a9cbd3775
Shipan Mazumder <shipanmazumder@gmail.com>
2024-09-16 15:46:07 +0600

    ...
    ..

--- src/main/java/com/shipan/cruddemo/dao/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shipan.cruddemo.dao;
-
-import com.shipan.cruddemo.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
